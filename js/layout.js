691d0e4e410a30ccaf370c705f9ae62fe0d35b1e
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-08-09 13:26:43 -0300

    Update: home and memorial

--- js/layout.js
+++ js/layout.js
@@ -18,7 +18,7 @@ function buildMenu() {
   menuContainer.innerHTML = `
     <nav class="site-menu">
       <ul class="menu-list">
-        <li class="menu-item"><a href="about.html" class="menu-link">Sobre</a></li>
+        <li class="menu-item"><a href="home.html" class="menu-link">Início</a></li>
         <li class="menu-item"><a href="faq.html" class="menu-link">FAQ</a></li>
         <li class="menu-item"><a href="gallery.html" class="menu-link">Galeria</a></li>
         <li class="menu-item"><a href="day.html" class="menu-link">23/05</a></li>
